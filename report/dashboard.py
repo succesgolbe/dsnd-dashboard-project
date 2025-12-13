7a694a920592fc4ddd91c4a2f719fe64dc191f98
succesgolbe <91133123+succesgolbe@users.noreply.github.com>
2025-12-13 15:35:40 -0500

    V13/12/26_1535

--- report/dashboard.py
+++ report/dashboard.py
@@ -29,23 +29,26 @@ class ReportDropdown(Dropdown):
     # Overwrite the build_component method
     # ensuring it has the same parameters
     # as the Report parent class's method
-    #### YOUR CODE HERE
+
+    def build_component(self, model):
         #  Set the `label` attribute so it is set
         #  to the `name` attribute for the model
-        #### YOUR CODE HERE
+        self.label = model.name
         
         # Return the output from the
         # parent class's build_component method
-        #### YOUR CODE HERE
+        return super().build_component(model)
     
     # Overwrite the `component_data` method
     # Ensure the method uses the same parameters
     # as the parent class method
-    #### YOUR CODE HERE
+    def component_data(self, model):
         # Using the model argument
         # call the employee_events method
         # that returns the user-type's
         # names and ids
+
+        return model.employee_events()
 
 
 # Create a subclass of base_components/BaseComponent
@@ -55,12 +58,12 @@ class Header(BaseComponent):
     # Overwrite the `build_component` method
     # Ensure the method has the same parameters
     # as the parent class
-    #### YOUR CODE HERE
+    def build_component(self, model):
         
         # Using the model argument for this method
         # return a fasthtml H1 objects
         # containing the model's name attribute
-        #### YOUR CODE HERE
+        return H1(model.name)
           
 
 # Create a subclass of base_components/MatplotlibViz
@@ -69,41 +72,40 @@ class LineChart(MatplotlibViz):
     
     # Overwrite the parent class's `visualization`
     # method. Use the same parameters as the parent
-    #### YOUR CODE HERE
+    def visualization(self, asset_id, model):
     
 
         # Pass the `asset_id` argument to
         # the model's `event_counts` method to
         # receive the x (Day) and y (event count)
-        #### YOUR CODE HERE
+        df = model.event_counts(asset_id)
         
         # Use the pandas .fillna method to fill nulls with 0
-        #### YOUR CODE HERE
+        df = df.fillna(0)
         
         # User the pandas .set_index method to set
         # the date column as the index
-        #### YOUR CODE HERE
+        df.set_index('event_date', inplace = True)
         
         # Sort the index
-        #### YOUR CODE HERE
+        df.sort_index(ascending=True, inplace=True)
         
         # Use the .cumsum method to change the data
         # in the dataframe to cumulative counts
-        #### YOUR CODE HERE
-        
+        df = df.cumsum()
         
         # Set the dataframe columns to the list
         # ['Positive', 'Negative']
-        #### YOUR CODE HERE
+        df.columns = ['Positive', 'Negative']
         
         # Initialize a pandas subplot
         # and assign the figure and axis
         # to variables
-        #### YOUR CODE HERE
+        fig, ax = plt.subplots()
         
         # call the .plot method for the
         # cumulative counts dataframe
-        #### YOUR CODE HERE
+        df.plot(ax=ax)
         
         # pass the axis variable
         # to the `.set_axis_styling`
@@ -112,10 +114,14 @@ class LineChart(MatplotlibViz):
         # the border color and font color to black. 
         # Reference the base_components/matplotlib_viz file 
         # to inspect the supported keyword arguments
-        #### YOUR CODE HERE
+        self.set_axis_styling(ax, border_color='black', font_color='black')
         
         # Set title and labels for x and y axis
-        #### YOUR CODE HERE
+        ax.set_title('Cumulative Positive and Negative Counts Over Time')
+        ax.set_xlabel('Day')
+        ax.set_ylabel('Event Count')
+
+        return fig
 
 
 # Create a subclass of base_components/MatplotlibViz
@@ -125,25 +131,25 @@ class BarChart(MatplotlibViz):
     # Create a `predictor` class attribute
     # assign the attribute to the output
     # of the `load_model` utils function
-    #### YOUR CODE HERE
+    predictor = load_model()
 
     # Overwrite the parent class `visualization` method
     # Use the same parameters as the parent
-    #### YOUR CODE HERE
+    def visualization(self, asset_id, model):
 
         # Using the model and asset_id arguments
         # pass the `asset_id` to the `.model_data` method
         # to receive the data that can be passed to the machine
         # learning model
-        #### YOUR CODE HERE
+        df = model.model_data(asset_id)
         
         # Using the predictor class attribute
         # pass the data to the `predict_proba` method
-        #### YOUR CODE HERE
+        model_output = self.predictor.predict_proba(df)
         
         # Index the second column of predict_proba output
         # The shape should be (<number of records>, 1)
-        #### YOUR CODE HERE
+        model_output = model_output[:,]
         
         
         # Below, create a `pred` variable set to
@@ -151,14 +157,16 @@ class BarChart(MatplotlibViz):
         #
         # If the model's name attribute is "team"
         # We want to visualize the mean of the predict_proba output
-        #### YOUR CODE HERE
+        if model.name == "team":
+            pred = model_output.mean()
             
         # Otherwise set `pred` to the first value
         # of the predict_proba output
-        #### YOUR CODE HERE
+        else:
+            pred = model_output[0]
         
         # Initialize a matplotlib subplot
-        #### YOUR CODE HERE
+        fig, ax = plt.subplots()
         
         # Run the following code unchanged
         ax.barh([''], [pred])
@@ -168,7 +176,9 @@ class BarChart(MatplotlibViz):
         # pass the axis variable
         # to the `.set_axis_styling`
         # method
-        #### YOUR CODE HERE
+        self.set_axis_styling(ax, border_color="black", font_color="black")
+
+        return fig
  
 # Create a subclass of combined_components/CombinedComponent
 # called Visualizations       
@@ -178,7 +188,7 @@ class Visualizations(CombinedComponent):
     # class attribute to a list
     # containing an initialized
     # instance of `LineChart` and `BarChart`
-    #### YOUR CODE HERE
+    children = [LineChart(), BarChart()]
 
     # Leave this line unchanged
     outer_div_type = Div(cls='grid')
@@ -189,12 +199,12 @@ class NotesTable(DataTable):
 
     # Overwrite the `component_data` method
     # using the same parameters as the parent class
-    #### YOUR CODE HERE
+    def component_data(self, model, entity_id):
         
         # Using the model and entity_id arguments
         # pass the entity_id to the model's .notes 
         # method. Return the output
-        #### YOUR CODE HERE
+        return model.notes(entity_id)
     
 
 class DashboardFilters(FormGroup):
@@ -224,24 +234,24 @@ class Report(CombinedComponents):
     # containing initialized instances 
     # of the header, dashboard filters,
     # data visualizations, and notes table
-    #### YOUR CODE HERE
+    children = [Header(), DashboardFilters(), Visualizations(), NotesTable()]
 
 # Initialize a fasthtml app 
-#### YOUR CODE HERE
+app = FastHTML()
 
 # Initialize the `Report` class
-#### YOUR CODE HERE
-
+report = Report()
 
 # Create a route for a get request
 # Set the route's path to the root
-#### YOUR CODE HERE
+@app.get("/")
+def homepage():
 
     # Call the initialized report
     # pass the integer 1 and an instance
     # of the Employee class as arguments
     # Return the result
-    #### YOUR CODE HERE
+    return report(1, Employee())
 
 # Create a route for a get request
 # Set the route's path to receive a request
@@ -250,13 +260,14 @@ class Report(CombinedComponents):
 # an ID of `2`. 
 # parameterize the employee ID 
 # to a string datatype
-#### YOUR CODE HERE
+@app.get("/employee/{id}")
+def employee_page(id:str):
 
     # Call the initialized report
     # pass the ID and an instance
     # of the Employee SQL class as arguments
     # Return the result
-    #### YOUR CODE HERE
+    return report(id, Employee())
 
 # Create a route for a get request
 # Set the route's path to receive a request
@@ -265,13 +276,14 @@ class Report(CombinedComponents):
 # an ID of `2`. 
 # parameterize the team ID 
 # to a string datatype
-#### YOUR CODE HERE
+@app.get("/team/{id}")
+def team_page(id:str):
 
     # Call the initialized report
     # pass the id and an instance
     # of the Team SQL class as arguments
     # Return the result
-    #### YOUR CODE HERE
+    return report(id, Team())
 
 
 # Keep the below code unchanged!
